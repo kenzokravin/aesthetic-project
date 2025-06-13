cf3c3133b4fa0cb6cb8a2a318e701e268562511c
kenzokravin <kenzokravin@gmail.com>
2025-06-13 14:14:37 +1000

    Creating Basic Boat Control Script.

--- Assets/Scripts/Transport/Boat/BoatController.cs
+++ Assets/Scripts/Transport/Boat/BoatController.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class BoatController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement Settings")]
+    public float acceleration = 10f;
+    public float turnTorque = 5f;
+    public float maxSpeed = 8f;
+    public float waterDrag = 1f;
+
+    public Rigidbody rb;
+
+    void Awake()
     {
-        
+
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.linearDamping = waterDrag;
+        rb.angularDamping = 2f; // Reduce spinning
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        float moveInput = Input.GetAxis("Vertical");   // W/S
+        float turnInput = Input.GetAxis("Horizontal"); // A/D
+
+        // Forward thrust
+        Vector3 forwardForce = transform.forward * moveInput * acceleration;
+        if (rb.linearVelocity.magnitude < maxSpeed)
+            rb.AddForce(forwardForce, ForceMode.Acceleration);
+
+        // Turning (torque on Y-axis)
+        rb.AddTorque(Vector3.up * turnInput * turnTorque, ForceMode.Acceleration);
     }
 }
+
+
+
